8785ebd080079d3faad0b05f381c83426b1cd570
Miles <38819342+mrSquidee@users.noreply.github.com>
2023-07-18 15:30:04 -0700

    dash mechanics

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,1 +1,9 @@
 extends CharacterBody2D
+
+@onready var mode: String = "Sand"
+
+func _on_mode_checker_body_entered(body):
+	mode = "Sand" 
+
+func _on_mode_checker_body_exited(body):
+	mode = "Water" 
