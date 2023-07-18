c04116bb8c853eb1d34a9451a07e0721935cdab9
Miles <38819342+mrSquidee@users.noreply.github.com>
2023-07-17 20:04:27 -0700

    deceleration bug fix

--- scripts/player/states/idle.gd
+++ scripts/player/states/idle.gd
@@ -1,5 +1,11 @@
 class_name Idle
 extends State
 
+const DECEL = 1000.0
+
 func enter():
 	animation_tree["parameters/playback"].travel("Idle")
+
+func handle_physics(delta: float):
+	character.velocity = character.velocity.move_toward(Vector2.ZERO, DECEL*delta)
+	character.move_and_slide()
