09a8bb61bad2db239762d0dc8080e323d655d074
Ian Lum <ianlum314@gmail.com>
2023-07-18 22:09:24 -0700

    switch to using events signals

--- scripts/player/player_state_machine.gd
+++ scripts/player/player_state_machine.gd
@@ -2,9 +2,6 @@ extends StateMachine
 
 @onready var dash_timer: Timer = $Dash/DashTimer
 
-signal enter_dialogue(npc_node)
-signal advance_dialogue
-
 var in_dialogue = false
 
 func _physics_process(delta: float) -> void:
@@ -13,10 +10,10 @@ func _physics_process(delta: float) -> void:
 		if not in_dialogue:
 			for area in get_parent().get_node("NPCDialogueCollider").get_overlapping_area():
 				if area.is_in_group("npc"):
-					emit_signal("enter_dialogue", area)
+					Events.emit_signal("enter_dialogue", area)
 					print(area.name)
 		else:
-			emit_signal("advance_dialogue")
+			Events.emit_signal("advance_dialogue")
 	elif Input.is_action_just_pressed("dash") or not dash_timer.is_stopped():
 		transition_to("Dash")
 	elif (
