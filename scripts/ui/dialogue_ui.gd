f4e519c92275688fbae1dae0b893621520ae5390
Ian Lum <ianlum314@gmail.com>
2023-07-18 22:08:34 -0700

    connect to all events dialogue signals

--- scripts/ui/dialogue_ui.gd
+++ scripts/ui/dialogue_ui.gd
@@ -5,6 +5,8 @@ extends CanvasLayer
 func _ready():
 	Events.alert_dialogue.connect(_on_alert_dialogue)
 	Events.idle_dialogue.connect(_on_idle_dialogue)
+	Events.enter_dialogue.connect(_on_enter_dialogue)
+	Events.advance_dialogue.connect(_on_advance_dialogue)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +14,13 @@ func _process(delta):
 	pass
 
 func _on_alert_dialogue():
-	pass
+	print('alert')
 
 func _on_idle_dialogue():
-	pass
+	print('idle')
+
+func _on_enter_dialogue(npc_node):
+	print('enter')
+
+func _on_advance_dialogue():
+	print('advance')
