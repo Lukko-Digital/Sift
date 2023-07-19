e4c9d5b3c46fac727517963806ead9d16039efbe
Ian Lum <ianlum314@gmail.com>
2023-07-18 22:29:06 -0700

    flip in_dialogue bool when entering dialogue

--- scripts/player/player_state_machine.gd
+++ scripts/player/player_state_machine.gd
@@ -10,6 +10,7 @@ func _physics_process(delta: float) -> void:
 			for area in get_parent().get_node("NPCDialogueCollider").get_overlapping_areas():
 				if area.is_in_group("npc"):
 					Events.emit_signal("enter_dialogue", area)
+					in_dialogue = true
 		else:
 			Events.emit_signal("advance_dialogue")
 	elif Input.is_action_just_pressed("dash") or not dash_timer.is_stopped():
