f5b9092ed48d8b771a0d9ff656ee7d2b3cd84246
Ian Lum <ianlum314@gmail.com>
2023-07-19 17:54:10 -0700

    add sample dialogue and load dialogue branches to npc script

--- scripts/npcs/npc.gd
+++ scripts/npcs/npc.gd
@@ -1,4 +1,16 @@
 extends Area2D
 
-@export var npc_name: String
-@export var dialogue_file_path: String
+@export var NPC_NAME: String
+@export var DIALOGUE_FILE: String
+
+var interaction_count: Dictionary
+
+func _ready():
+	var dialogue_path = "res://assets/dialogue/%s" % DIALOGUE_FILE
+	assert(FileAccess.file_exists(dialogue_path), "Dialog file at %s does not exist" % dialogue_path)
+	for branch in JSON.parse_string(
+		FileAccess.open(
+			dialogue_path, FileAccess.READ
+		).get_as_text()
+	)["branches"]:
+		interaction_count[branch] = 0
