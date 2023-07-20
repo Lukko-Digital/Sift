33a79f3dba65e2c57bc2750f6b329189d1478261
Ian Lum <ianlum314@gmail.com>
2023-07-19 21:23:33 -0700

    basic dialogue cycling ISSUES: doesnt re-alert when exiting dialogue SOLUTION: emit alert dialogue and make a new dialogue complete signal that ping the player to set `in_dialogue` to false and also pings the npc to tick `interaction_count`

--- scripts/ui/states/Dialogue.gd
+++ scripts/ui/states/Dialogue.gd
@@ -1,8 +1,11 @@
 extends State
 
 @export var dialogue_box: NinePatchRect
+@onready var dialogue_label = dialogue_box.get_node("Dialogue")
 
 var current_npc
+var current_dialogue_tree: Dictionary
+var current_dialogue_display: Dictionary
 
 func _ready():
 	Events.enter_dialogue.connect(_on_enter_dialogue)
@@ -10,6 +13,10 @@ func _ready():
 
 func enter():
 	dialogue_box.show()
+	var dialogue_path = "res://assets/dialogue/%s" % current_npc.DIALOGUE_FILE
+	current_dialogue_tree = JSON.parse_string(FileAccess.open(dialogue_path, FileAccess.READ).get_as_text())
+	current_dialogue_display = current_dialogue_tree["O0.0"]
+	dialogue_label.text = current_dialogue_display["text"]
 
 func exit():
 	dialogue_box.hide()
@@ -18,4 +25,9 @@ func _on_enter_dialogue(npc_node):
 	current_npc = npc_node
 
 func _on_advance_dialogue():
-	pass
+	var next_dialogue_id = current_dialogue_display["next"]
+	if next_dialogue_id != "EXIT":
+		current_dialogue_display = current_dialogue_tree[next_dialogue_id]
+		dialogue_label.text = current_dialogue_display["text"]
+	else:
+		Events.emit_signal("idle_dialogue")
