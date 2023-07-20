89be014228504f98b6ce371ae89ab4a52ee05520
Ian Lum <ianlum314@gmail.com>
2023-07-19 19:44:41 -0700

    switch dialogue ui to statemachine layout

--- scripts/ui/dialogue_ui_state_machine.gd
+++ scripts/ui/dialogue_ui_state_machine.gd
@@ -1,15 +1,11 @@
-extends CanvasLayer
+extends StateMachine
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Events.alert_dialogue.connect(_on_alert_dialogue)
 	Events.idle_dialogue.connect(_on_idle_dialogue)
 	Events.enter_dialogue.connect(_on_enter_dialogue)
 	Events.advance_dialogue.connect(_on_advance_dialogue)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
