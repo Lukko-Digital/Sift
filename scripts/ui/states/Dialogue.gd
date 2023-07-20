ec116d8b3b1ace845138ffd1920f838bac4745bd
Ian Lum <ianlum314@gmail.com>
2023-07-19 21:02:18 -0700

    remember current npc

--- scripts/ui/states/Dialogue.gd
+++ scripts/ui/states/Dialogue.gd
@@ -2,16 +2,20 @@ extends State
 
 @export var dialogue_box: NinePatchRect
 
-func enter():
-	dialogue_box.show()
+var current_npc
+
+func _ready():
 	Events.enter_dialogue.connect(_on_enter_dialogue)
 	Events.advance_dialogue.connect(_on_advance_dialogue)
+
+func enter():
+	dialogue_box.show()
 
 func exit():
 	dialogue_box.hide()
 
 func _on_enter_dialogue(npc_node):
-	pass
+	current_npc = npc_node
 
 func _on_advance_dialogue():
 	pass
