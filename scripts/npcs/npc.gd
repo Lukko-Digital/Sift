4df3630242af014781b6e3bad824a89ac4f14307
Ian Lum <ianlum314@gmail.com>
2023-07-20 19:25:44 -0700

    implement branch interaction limit dict creation

--- scripts/npcs/npc.gd
+++ scripts/npcs/npc.gd
@@ -4,18 +4,34 @@ extends Area2D
 @export var DIALOGUE_FILE: String
 
 var interaction_count: Dictionary
+var branch_interaction_limits: Dictionary
 
 func _ready():
 	Events.interaction_complete.connect(_on_interaction_complete)
 	
 	var dialogue_path = "res://assets/dialogue/%s" % DIALOGUE_FILE
 	assert(FileAccess.file_exists(dialogue_path), "Dialog file at %s does not exist" % dialogue_path)
-	for branch in JSON.parse_string(
+	var dialogue_json = JSON.parse_string(
 		FileAccess.open(
 			dialogue_path, FileAccess.READ
 		).get_as_text()
-	)["branches"]:
+	)
+	for branch in dialogue_json["branches"]:
 		interaction_count[branch] = 0
+	
+	for branch in dialogue_json["branches"]:
+		branch_interaction_limits[branch] = []
+		
+	var dialogue_ids = []
+	for id in dialogue_json.keys():
+		if id == "branches":
+			continue
+		var split_id = id.rsplit(".")
+		var branch = split_id[0]
+		var limit = int(split_id[1])
+		if limit not in branch_interaction_limits[branch]:
+			branch_interaction_limits[branch].append(limit)
 
 func _on_interaction_complete(npc_node, branch):
-	pass
+	if npc_node == self:
+		interaction_count[branch] += 1
