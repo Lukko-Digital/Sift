f88bb5732512b1498b8de22041d41f84c6958021
Ian Lum <ianlum314@gmail.com>
2023-07-20 19:28:22 -0700

    refactor ready functions

--- scripts/npcs/npc.gd
+++ scripts/npcs/npc.gd
@@ -9,16 +9,24 @@ var branch_interaction_limits: Dictionary
 func _ready():
 	Events.interaction_complete.connect(_on_interaction_complete)
 	
+	var dialogue_json = load_dialogue_json()
+	init_iteraction_count(dialogue_json)
+	init_branch_interaction_limits(dialogue_json)
+
+func load_dialogue_json():
 	var dialogue_path = "res://assets/dialogue/%s" % DIALOGUE_FILE
 	assert(FileAccess.file_exists(dialogue_path), "Dialog file at %s does not exist" % dialogue_path)
-	var dialogue_json = JSON.parse_string(
+	return JSON.parse_string(
 		FileAccess.open(
 			dialogue_path, FileAccess.READ
 		).get_as_text()
 	)
+
+func init_iteraction_count(dialogue_json):
 	for branch in dialogue_json["branches"]:
 		interaction_count[branch] = 0
 	
+func init_branch_interaction_limits(dialogue_json):
 	for branch in dialogue_json["branches"]:
 		branch_interaction_limits[branch] = []
 		
@@ -31,6 +39,7 @@ func _ready():
 		var limit = int(split_id[1])
 		if limit not in branch_interaction_limits[branch]:
 			branch_interaction_limits[branch].append(limit)
+
 
 func _on_interaction_complete(npc_node, branch):
 	if npc_node == self:
