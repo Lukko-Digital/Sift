f19daf53756f44f1a5ea6a1fc8c90f3e39749911
Ian <ianlum314@gmail.com>
2023-07-23 12:59:52 -0700

    add tracking radius the crab will only begin tracking when player enters aggro radius, but will continue to track as long as player is in tracking radius

--- scripts/enemies/crab/crab_state_machine.gd
+++ scripts/enemies/crab/crab_state_machine.gd
@@ -1,14 +1,17 @@
 extends StateMachine
 
-@onready var aggro_raidus: Area2D = get_parent().get_node("AggroRadius")
-@onready var attack_raidus: Area2D = get_parent().get_node("AttackRadius")
+@onready var aggro_raidus: Area2D = get_node("../AggroRadius")
+@onready var tracking_radius: Area2D = get_node("../TrackingRadius")
+@onready var attack_raidus: Area2D = get_node("../AttackRadius")
 
 func _physics_process(delta: float) -> void:
 	if not attack_raidus.get_overlapping_bodies().is_empty():
 		transition_to("Attack")
-	elif not aggro_raidus.get_overlapping_bodies().is_empty():
+	elif not aggro_raidus.get_overlapping_bodies().is_empty() or (
+		state.name == "Track" and not tracking_radius.get_overlapping_bodies().is_empty()
+	):
 		transition_to("Track")
 	else:
 		transition_to("Idle")
-	
+
 	state.handle_physics(delta)	
