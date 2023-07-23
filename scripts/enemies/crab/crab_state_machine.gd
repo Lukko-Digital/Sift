a3e528ec1b9ab839404f58ba397b414074ca3028
Ian <ianlum314@gmail.com>
2023-07-22 17:17:13 -0700

    configure state switching logic

--- scripts/enemies/crab/crab_state_machine.gd
+++ scripts/enemies/crab/crab_state_machine.gd
@@ -1,1 +1,12 @@
 extends StateMachine
+
+@onready var aggro_raidus: Area2D = get_parent().get_node("AggroRadius")
+@onready var attack_raidus: Area2D = get_parent().get_node("AttackRadius")
+
+func _physics_process(delta: float) -> void:
+	if not attack_raidus.get_overlapping_bodies().is_empty():
+		transition_to("Attack")
+	elif not aggro_raidus.get_overlapping_bodies().is_empty():
+		transition_to("Track")
+	else:
+		transition_to("Idle")
