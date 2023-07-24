8045f6f75ad6a872474f6206a0a4f8de38ed6c58
Ian <ianlum314@gmail.com>
2023-07-23 17:17:11 -0700

    spell radius correctly

--- scripts/enemies/crab/crab_state_machine.gd
+++ scripts/enemies/crab/crab_state_machine.gd
@@ -1,17 +1,17 @@
 extends StateMachine
 
-@onready var aggro_raidus: Area2D = get_node("../AggroRadius")
+@onready var aggro_radius: Area2D = get_node("../AggroRadius")
 @onready var tracking_radius: Area2D = get_node("../TrackingRadius")
-@onready var attack_raidus: Area2D = get_node("../AttackRadius")
+@onready var attack_radius: Area2D = get_node("../AttackRadius")
 @onready var attack_timer: Timer = $Attack/AttackTimer
 
 func _physics_process(delta: float) -> void:
 	if (
-		not attack_raidus.get_overlapping_bodies().is_empty() or 
+		not attack_radius.get_overlapping_bodies().is_empty() or 
 		not attack_timer.is_stopped()
 	):
 		transition_to("Attack")
-	elif not aggro_raidus.get_overlapping_bodies().is_empty() or (
+	elif not aggro_radius.get_overlapping_bodies().is_empty() or (
 		state.name == "Track" and not tracking_radius.get_overlapping_bodies().is_empty()
 	):
 		transition_to("Track")
