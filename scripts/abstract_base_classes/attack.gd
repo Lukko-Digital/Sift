eb9801a617d12701b3ebd96e6897fb9cd0bf918d
Ian <ianlum314@gmail.com>
2023-07-31 22:05:07 -0700

    attack defaults to no effect

--- scripts/abstract_base_classes/attack.gd
+++ scripts/abstract_base_classes/attack.gd
@@ -4,7 +4,7 @@ var name: String
 var damage: int
 var effects: Array[Effect]
 
-func _init(name: String, damage: int, effects = Array[Effect]):
+func _init(name: String, damage: int, effects: Array[Effect] = []):
 	self.name = name
 	self.damage = damage
 	self.effects = effects
