f1739f943e76b75eecb97b253b10699e3fb16527
Ian Lum <ianlum314@gmail.com>
2023-08-01 18:32:05 -0700

    implement knockback state

--- scripts/enemies/crab/states/knocked_back.gd
+++ scripts/enemies/crab/states/knocked_back.gd
@@ -1,5 +1,17 @@
 class_name KnockedBack
 extends State
 
+var duration: float
+var direction: Vector2
+
+@onready var knockback_timer = $KnockBackTimer
+
+func recieve_args(effect):
+	self.duration = effect.duration
+	self.direction = effect.direction
+
 func enter():
-	pass
+	knockback_timer.start(duration)
+
+func handle_physics(delta: float):
+	character.velocity = KnockedBackEffect.KNOCK_BACK_SPEED * direction.normalized()
