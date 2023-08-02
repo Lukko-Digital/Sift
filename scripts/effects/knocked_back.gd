09e96a37e0bbdcfda6116949211d8354e877c380
Ian Lum <ianlum314@gmail.com>
2023-08-01 18:00:38 -0700

    create knocked back effect

---
+++ scripts/effects/knocked_back.gd
@@ -0,0 +1,12 @@
+class_name KnockedBackEffect
+extends Effect
+
+const KNOCK_BACK_SPEED = 50
+
+var duration: float
+var direction: Vector2
+
+func _init(duration: float):
+	self.duration = duration
+	self.direction = direction
+	effect_name = Effect.EffectName.KNOCKED_BACK
