b2e08c78bc7a696fd4f85869529d730821ae7515
Ian Lum <ianlum314@gmail.com>
2023-08-07 22:57:34 -0700

    make hurtbox not monitorable when dead

--- scripts/enemies/crab/states/dead.gd
+++ scripts/enemies/crab/states/dead.gd
@@ -6,6 +6,8 @@ const NUM_DEAD_ANIMATIONS = 2
 
 func enter():
 	var hit_box: CollisionShape2D = character.get_node("HitBox")
+	var hurtbox: HurtboxComponent = character.get_node("HurtboxComponent")
 	hit_box.disabled = true
+	hurtbox.monitorable = false
 	var anim_idx = randi() % NUM_DEAD_ANIMATIONS + 1
 	animation_player.play("Dead_%s" % anim_idx)
