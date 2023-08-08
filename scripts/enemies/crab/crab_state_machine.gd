1a9e8afb44a94f897cd8ff33680d88e5d58a2bb0
Ian Lum <ianlum314@gmail.com>
2023-08-07 19:50:35 -0700

    implement stun handling

--- scripts/enemies/crab/crab_state_machine.gd
+++ scripts/enemies/crab/crab_state_machine.gd
@@ -9,6 +9,7 @@ extends StateMachine
 @onready var health_component: HealthComponent = get_node("../HealthComponent")
 @onready var hurtbox_component: HurtboxComponent = get_node("../HurtboxComponent")
 @onready var knockback_timer: Timer = $KnockedBack/KnockBackTimer
+@onready var stun_timer: Timer = $StunTimer
 
 var facing_direction: Vector2 = Vector2(0, 1)
 var is_dead = false
@@ -27,6 +28,8 @@ func _physics_process(delta: float) -> void:
 		transition_to("KnockedUp")
 	elif not knockback_timer.is_stopped():
 		transition_to("KnockedBack")
+	elif not stun_timer.is_stopped():
+		transition_to("Idle")
 	elif (
 		not attack_radius.get_overlapping_bodies().is_empty() or 
 		animation_player.current_animation == "Attack_front" or
@@ -61,3 +64,5 @@ func _on_damage_taken(effects):
 			transition_to("KnockedUp")
 		elif effect.effect_name == Effect.EffectName.KNOCKED_BACK:
 			transition_to("KnockedBack", effect)
+		elif effect.effect_name == Effect.EffectName.STUNNED:
+			stun_timer.start(effect.duration)
