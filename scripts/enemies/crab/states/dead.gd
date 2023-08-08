7b78bd6d1e406fb5cb060ad0d0f39c88577339f9
Ian Lum <ianlum314@gmail.com>
2023-08-07 22:12:15 -0700

    randomly choose dead animation on dead

--- scripts/enemies/crab/states/dead.gd
+++ scripts/enemies/crab/states/dead.gd
@@ -1,6 +1,11 @@
 extends State
 
+@export var animation_player: AnimationPlayer
+
+const NUM_DEAD_ANIMATIONS = 2
+
 func enter():
-	character.modulate = Color(Color.BLACK)
 	var hit_box: CollisionShape2D = character.get_node("HitBox")
 	hit_box.disabled = true
+	var anim_idx = randi() % NUM_DEAD_ANIMATIONS + 1
+	animation_player.play("Dead_%s" % anim_idx)
