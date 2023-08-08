ff9c7f0f529332b4f3e45afc9c4925ea0ac82bc4
Ian <ianlum314@gmail.com>
2023-08-08 13:47:38 -0700

    refactor dead state

--- scripts/enemies/crab/states/dead.gd
+++ scripts/enemies/crab/states/dead.gd
@@ -6,10 +6,16 @@ extends State
 const NUM_DEAD_ANIMATIONS = 2
 
 func enter():
+	disable_hitboxes()
+	play_animations()
+
+func disable_hitboxes():
 	var hit_box: CollisionShape2D = character.get_node("HitBox")
 	var hurtbox: HurtboxComponent = character.get_node("HurtboxComponent")
 	hit_box.disabled = true
 	hurtbox.monitorable = false
+
+func play_animations():
 	var anim_idx = randi() % NUM_DEAD_ANIMATIONS + 1
 	animation_player.play("Dead_%s" % anim_idx)
 	color_animation_player.play("On_hit_white")
