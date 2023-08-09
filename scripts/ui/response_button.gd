0a34e00ed8a6b870e743ba044ad607280ffa144c
Ian <ianlum314@gmail.com>
2023-08-09 12:45:58 -0700

    disable response button on spawn for duration

--- scripts/ui/response_button.gd
+++ scripts/ui/response_button.gd
@@ -1,9 +1,18 @@
 class_name ResponseButton
 extends Button
 
+const UNPRESSABLE_DURATION = 1
+
 signal response_pressed(destination_branch)
 
+@onready var spawn_timer: Timer = $SpawnTimer
+
 var destination_branch: String
+
+func _ready():
+	spawn_timer.start(UNPRESSABLE_DURATION)
+	await spawn_timer.timeout
+	self.disabled = false
 
 func _pressed():
 	emit_signal("response_pressed", destination_branch)
