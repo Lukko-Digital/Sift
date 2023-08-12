5e406bb3b672ba6985e422688c4c7b9d92a9847e
Miles <38819342+mrSquidee@users.noreply.github.com>
2023-08-12 16:55:37 -0700

    better instantiation of sand attack

--- scripts/player/states/sand_attack.gd
+++ scripts/player/states/sand_attack.gd
@@ -35,7 +35,7 @@ func enter():
 	
 	var instance = sand_attack_scene.instantiate()
 	instance.start(global_position + Vector2(0, -12), anim_name)
-	get_node("../../..").add_child(instance)
+	find_parent("player").get_parent().add_child(instance)
 	Global.camera.shake(0.1, 2)
 	
 func handle_physics(delta):
