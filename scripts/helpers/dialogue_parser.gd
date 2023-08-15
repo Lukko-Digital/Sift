1917720b3d04e37c8ad15bed99dd460f3ef0e86d
Ian <ianlum314@gmail.com>
2023-08-14 23:58:27 -0400

    get name and image from first two lines

--- scripts/helpers/dialogue_parser.gd
+++ scripts/helpers/dialogue_parser.gd
@@ -4,8 +4,27 @@ extends Node
 static func b(dialogue_file):
 	var dialogue_path = "res://assets/dialogue/%s" % dialogue_file
 	assert(FileAccess.file_exists(dialogue_path), "Dialog file at %s does not exist" % dialogue_path)
-	var raw = FileAccess.open(
+	var lines = FileAccess.open(
 		dialogue_path, FileAccess.READ
-	).get_as_text()
-	var lines = raw.split('\n')
+	).get_as_text().split('\n')
 	print(lines)
+
+	var dialogue_dict = {
+		"info": {
+			"name": "",
+			"image": "",
+		},
+		"branches": {}
+	}
+
+	# Get name and image from first two lines
+	assert(
+		lines[0].substr(0,6) == "name: ",
+		"First line must be in the format: `name: [NAME]`"
+	)
+	assert(
+		lines[1].substr(0,7) == "image: ",
+		"Second line must be in the format: image: [IMAGE FILE PATH]`"
+	)
+	dialogue_dict["info"]["name"] = lines[0].substr(6)
+	dialogue_dict["info"]["image"] = lines[1].substr(7)
