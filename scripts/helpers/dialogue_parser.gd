ad9785c8049acd1fbef1947c461b2f6ce2dd1ac0
Ian <ianlum314@gmail.com>
2023-08-15 16:53:20 -0400

    parse lines through `match` statement of first 2 chars

--- scripts/helpers/dialogue_parser.gd
+++ scripts/helpers/dialogue_parser.gd
@@ -31,28 +31,54 @@ static func b(dialogue_file):
 	var dialogue_tree: Dictionary
 	var branch: String
 	var interaction: String
-	var dialogue_idx: int = 0
 
-	for line in lines:
+	# Loop through lines starting on the 3rd
+	for line in lines.slice(2):
 		# Remove tabs
 		line = line.replace("\t","")
-		# Check if new branch begins
-		if line.substr(0,9) == "~ branch ":
-			branch = line.substr(9)
-			dialogue_tree[branch] = {}
-		# If there is no branch, pass
-		elif not branch:
-			continue
-		# Get interaction number if line ends in colon
-		elif line[-1] == ":":
-			interaction = line.substr(0, len(line) - 1)
-			dialogue_tree[branch][interaction] = []
-			dialogue_idx = 0
-		# Check if line is a response option
-		elif line.substr(0,2) == "- ":
-			pass
-		# Else, assume that the line is a dialogue item
-		else:
-			dialogue_tree[branch][interaction] = line
+		# Match the first 2 characters, the second should always be a space
+		match line.substr(0,2):
+			# Branch
+			"~ ":
+				branch = line.substr(2)
+				dialogue_tree[branch] = {}
+			# Interaction
+			": ":
+				interaction = line.substr(2)
+				assert(
+					dialogue_tree.has(branch),
+					"No branch detected. Branches are denoted `~ [BRANCH_ID]`"
+				)
+				dialogue_tree[branch][interaction] = []
+			# Response
+			"- ":
+				var dialogue = dialogue_tree[branch][interaction][-1]
+				if not dialogue.has("responses"):
+					dialogue["responses"] = {}
+				
+				var split_line = line.substr(2).split(" > ")
+				var text = split_line[0]
+				var next = split_line[1]
+				
+				var response_num = len(dialogue["responses"]) + 1
+				
+				dialogue["responses"][response_num] = {
+					"text": text,
+					"next": next
+				}
+			# Switch
+			"> ":
+				pass
+			# Dialogue
+			_:
+				assert(
+					dialogue_tree.has(branch),
+					"No branch detected. Branches are denoted `~ [BRANCH_ID]`"
+				)
+				assert(
+					dialogue_tree[branch].has(interaction),
+					"Interaction path not detected. Interaction paths are denoted `: [MIN_INTERACRTIONS_FOR_PATH]`"
+				)
+				dialogue_tree[branch][interaction].append({"text": line})
 	
 	print(dialogue_tree)
