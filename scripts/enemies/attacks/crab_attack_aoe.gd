9a588050eb3bbcc06db9d0fe282ed35c57d7e1cf
Ian <ianlum314@gmail.com>
2023-09-15 14:27:25 -0400

    remove unused functions

--- scripts/enemies/attacks/crab_attack_aoe.gd
+++ scripts/enemies/attacks/crab_attack_aoe.gd
@@ -29,24 +29,6 @@ func _on_animation_end(anim_name: StringName):
 	if anim_name == "Attack_front":
 		animation_player.play("Idle_front")
 		attack_timer.start(END_LAG)
-	
-func get_direction_to_player():
-	for body in attack_radius.get_overlapping_bodies():
-		if body.name == "player":
-			return (body.global_position - character.global_position).normalized()
-	return Vector2.ZERO
-
-func place_attack_box():
-	# place attack box for directional attack
-	var vec_to_player: Vector2 = get_direction_to_player()
-	var pos: Vector2
-	if abs(vec_to_player.x) > abs(vec_to_player.y):
-		# Left Right
-		pos = Vector2(sign(vec_to_player.x)*HORIZONTAL_ATTACK_PLACEMENT, 0)
-	else:
-		# Up Down
-		pos = Vector2(0, sign(vec_to_player.y)*VERTICAL_ATTACK_PLACEMENT)
-	attack_collider.position = pos
 
 func _on_hit(area):
 	if area.is_in_group("player_hurtbox"):
