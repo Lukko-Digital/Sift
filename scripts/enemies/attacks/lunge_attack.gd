c6a43da6452396ae2a97462075498b2edb170584
Ian <ianlum314@gmail.com>
2023-09-16 14:07:34 -0400

    implement windup and endlag for lunge

--- scripts/enemies/attacks/lunge_attack.gd
+++ scripts/enemies/attacks/lunge_attack.gd
@@ -1,22 +1,40 @@
 extends State
 
+@export var animation_player: AnimationPlayer
+
 const LUNGE_SPEED = 200
+const LUNGE_DURATION = 0.2
+const END_LAG = 0.9
 
 @onready var attack_radius: Area2D = $AttackRadius
+@onready var lunge_timer: Timer = $LungeTimer
+@onready var end_lag_timer: Timer = $EndLag
+
 var vec_to_player: Vector2
 
 func _ready():
-	pass
+	animation_player.animation_finished.connect(_on_animation_end)
+	lunge_timer.timeout.connect(_on_lunge_end)
 
 func enter():
+	character.velocity = Vector2()
 	vec_to_player = get_direction_to_player()
+	animation_player.play("Attack_windup")
 	
 func handle_physics(delta: float):
-	character.velocity = vec_to_player * LUNGE_SPEED
 	character.move_and_slide()
 
 func exit():
 	pass
+
+func _on_animation_end(anim_name: StringName):
+	if anim_name == "Attack_windup":
+		lunge_timer.start(LUNGE_DURATION)
+		character.velocity = vec_to_player * LUNGE_SPEED
+
+func _on_lunge_end():
+	end_lag_timer.start(END_LAG)
+	character.velocity = Vector2()
 
 func get_direction_to_player():
 	for body in attack_radius.get_overlapping_bodies():
--- scripts/enemies/state_machines/lunge_enemy_state_machine.gd
+++ scripts/enemies/state_machines/lunge_enemy_state_machine.gd
@@ -6,7 +6,8 @@ extends StateMachine
 @onready var aggro_radius: Area2D = $Track/AggroRadius
 @onready var tracking_radius: Area2D = $Track/TrackingRadius
 @onready var attack_radius: Area2D = $Attack/AttackRadius
-@onready var attack_timer: Timer = $Attack/AttackTimer
+@onready var attack_timer: Timer = $Attack/LungeTimer
+@onready var end_lag_timer: Timer = $Attack/EndLag
 @onready var health_component: HealthComponent = get_node("../HealthComponent")
 @onready var hurtbox_component: HurtboxComponent = get_node("../HurtboxComponent")
 @onready var knockback_timer: Timer = $KnockedBack/KnockBackTimer
@@ -17,7 +18,7 @@ var is_dead = false
 
 func _ready():
 	super._ready()
-	attack_timer.timeout.connect(_return_to_idle)
+	end_lag_timer.timeout.connect(_return_to_idle)
 	health_component.died.connect(_on_death)
 	health_component.damage_taken.connect(_on_damage_taken)
 	animation_player.animation_finished.connect(_on_animation_finished)
@@ -31,10 +32,9 @@ func _physics_process(delta: float) -> void:
 		transition_to("Idle")
 	elif (
 		not attack_radius.get_overlapping_bodies().is_empty() or 
-#		animation_player.current_animation in [
-#			"Attack_up", "Attack_right", "Attack_down", "Attack_left"
-#			] or
-		not attack_timer.is_stopped()
+		animation_player.current_animation in ["Attack_windup"] or
+		not attack_timer.is_stopped() or 
+		not end_lag_timer.is_stopped()
 	):
 		transition_to("Attack")
 	elif (
