91f2146c4d30f36ebbf7e25670c4e221d61b1c47
Ian <ianlum314@gmail.com>
2023-09-16 13:16:24 -0400

    implement dash to player no cooldown, enemy moves in a straight line in the direction of the player

--- scripts/enemies/attacks/lunge_attack.gd
+++ scripts/enemies/attacks/lunge_attack.gd
@@ -1,10 +1,25 @@
 extends State
 
+const LUNGE_SPEED = 200
+
+@onready var attack_radius: Area2D = $AttackRadius
+var vec_to_player: Vector2
+
 func _ready():
 	pass
 
 func enter():
-	pass
+	vec_to_player = get_direction_to_player()
+	
+func handle_physics(delta: float):
+	character.velocity = vec_to_player * LUNGE_SPEED
+	character.move_and_slide()
 
 func exit():
 	pass
+
+func get_direction_to_player():
+	for body in attack_radius.get_overlapping_bodies():
+		if body.name == "player":
+			return (body.global_position - character.global_position).normalized()
+	return Vector2.ZERO
