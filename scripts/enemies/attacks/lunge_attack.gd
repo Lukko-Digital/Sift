56db6647db4d9e5e9ba2deb7925a2ce7746641d0
Ian <ianlum314@gmail.com>
2023-09-16 15:19:10 -0400

    add permanently active attack box

--- scripts/enemies/attacks/lunge_attack.gd
+++ scripts/enemies/attacks/lunge_attack.gd
@@ -7,14 +7,17 @@ const LUNGE_DISTANCE = 100.
 const END_LAG = 0.9
 
 @onready var attack_radius: Area2D = $AttackRadius
+@onready var attack_box: Area2D = $AttackBox
 @onready var lunge_timer: Timer = $LungeTimer
 @onready var end_lag_timer: Timer = $EndLag
 
 var vec_to_player: Vector2
+var lunge_attack: Attack = Attack.new("lunge", 1)
 
 func _ready():
+	attack_box.area_entered.connect(_on_hit)
 	animation_player.animation_finished.connect(_on_animation_end)
-	lunge_timer.timeout.connect(_on_lunge_end)
+	lunge_timer.timeout.connect(on_lunge_end)
 
 func enter():
 	character.velocity = Vector2()
@@ -32,9 +35,18 @@ func _on_animation_end(anim_name: StringName):
 		lunge_timer.start(LUNGE_DISTANCE / LUNGE_SPEED)
 		character.velocity = vec_to_player * LUNGE_SPEED
 
-func _on_lunge_end():
+func on_lunge_end():
 	end_lag_timer.start(END_LAG)
 	character.velocity = Vector2()
+
+func _on_hit(area):
+	on_lunge_end()
+	if area.is_in_group("player_hurtbox"):
+		lunge_attack.effects = [
+			KnockedBackEffect.new(0.1, area.global_position-self.global_position),
+			StunnedEffect.new(0.25),
+		]
+		area.damage(lunge_attack)
 
 func get_direction_to_player():
 	for body in attack_radius.get_overlapping_bodies():
