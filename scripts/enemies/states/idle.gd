edf8d6b69b7260ed950b6a236b1e5c43d7a04b35
Ian <ianlum314@gmail.com>
2023-09-18 21:27:57 -0400

    implement playing idle animation for lunge enemy idle state now checks if animation exists before playing

--- scripts/enemies/states/idle.gd
+++ scripts/enemies/states/idle.gd
@@ -4,10 +4,17 @@ extends State
 @onready var state_machine: StateMachine = get_parent()
 
 func enter():
-	if state_machine.facing_direction.y > 0:
-		animation_player.play("Idle_front")
-	else:
-		animation_player.play("Idle_back")
+	if animation_player.has_animation("Idle_front") and animation_player.has_animation("Idle_back"):
+		if state_machine.facing_direction.y > 0:
+			animation_player.play("Idle_front")
+		else:
+			animation_player.play("Idle_back")
+	
+	if animation_player.has_animation("Idle_right") and animation_player.has_animation("Idle_left"):
+		if state_machine.facing_direction.x > 0:
+			animation_player.play("Idle_right")
+		else:
+			animation_player.play("Idle_left")
 
 func exit():
 	animation_player.stop()
