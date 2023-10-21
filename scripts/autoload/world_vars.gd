bd8ee15c01afeccfeec73974ceb30d1c274385b4
Ian <ianlum314@gmail.com>
2023-10-21 17:40:58 -0400

    remove commented lines

--- scripts/autoload/world_vars.gd
+++ scripts/autoload/world_vars.gd
@@ -9,14 +9,6 @@ func _ready():
 	Events.set_world_var.connect(_set_world_var)
 	Events.increment_world_var.connect(_increment_world_var)
 
-#	var e = Expression.new()
-#	e.parse("""emit_signal("set_world_var","A",10)""")
-#	e.execute([], Events)
-#
-#	var f = Expression.new()
-#	f.parse("""emit_signal("increment_world_var","B",3)""")
-#	f.execute([], Events)
-
 func _set_world_var(key, val):
 	world_vars[key] = val
 
