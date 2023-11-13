74611ad0466ca840dc9404e67275861dd331aa6d
Ian <ianlum314@gmail.com>
2023-11-13 11:17:53 -0500

    projectile does damage

--- scripts/enemies/attacks/projectile.gd
+++ scripts/enemies/attacks/projectile.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 const SPEED = 200
 
+@onready var hitbox: Area2D = $HitBox
+
+var projectile_attack: Attack = Attack.new("projectile attack", 1)
 var direction: Vector2
 
 func start(start_pos: Vector2, dir: Vector2):
@@ -9,7 +12,16 @@ func start(start_pos: Vector2, dir: Vector2):
 	direction = dir
 
 func _ready():
-	pass
+	hitbox.area_entered.connect(_on_hit)
 
 func _process(delta):
 	position += direction * SPEED * delta
+
+func _on_hit(area):
+	if area.is_in_group("player_hurtbox"):
+		projectile_attack.effects = [
+			KnockedBackEffect.new(0.1, 200, area.global_position-self.global_position),
+			StunnedEffect.new(0.25),
+		]
+		area.damage(projectile_attack)
+		queue_free()
