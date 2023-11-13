e070e4ff3c54c4b15c62f6a51368d85c0c2fb30a
Ian <ianlum314@gmail.com>
2023-11-13 11:34:52 -0500

    projectile despawns after duration

--- scripts/enemies/attacks/projectile.gd
+++ scripts/enemies/attacks/projectile.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 const SPEED = 200
+const DESPAWN_TIME = 8
 
 @onready var hitbox: Area2D = $HitBox
+@onready var despawn_timer: Timer = $DespawnTimer
 
 var projectile_attack: Attack = Attack.new("projectile attack", 1)
 var direction: Vector2
@@ -13,6 +15,8 @@ func start(start_pos: Vector2, dir: Vector2):
 
 func _ready():
 	hitbox.area_entered.connect(_on_hit)
+	despawn_timer.timeout.connect(despawn)
+	despawn_timer.start(DESPAWN_TIME)
 
 func _process(delta):
 	position += direction * SPEED * delta
@@ -24,4 +28,7 @@ func _on_hit(area):
 			StunnedEffect.new(0.25),
 		]
 		area.damage(projectile_attack)
-		queue_free()
+		despawn()
+
+func despawn():
+	queue_free()
