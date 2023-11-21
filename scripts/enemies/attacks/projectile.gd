463af3fb44d3ecf8e420721ce10ebf8ceb24ac2e
Ian Lum <ianlum314@gmail.com>
2023-11-20 16:06:04 -0800

    add projectile animation

--- scripts/enemies/attacks/projectile.gd
+++ scripts/enemies/attacks/projectile.gd
@@ -3,6 +3,8 @@ extends Node2D
 const SPEED = 200
 const DESPAWN_TIME = 8
 
+@export var animation_player: AnimationPlayer
+
 @onready var hitbox: Area2D = $HitBox
 @onready var despawn_timer: Timer = $DespawnTimer
 
@@ -12,8 +14,11 @@ var direction: Vector2
 func start(start_pos: Vector2, dir: Vector2):
 	position = start_pos
 	direction = dir
+	look_at(direction)
+	animation_player.play("fly")
 
 func _ready():
+	animation_player.animation_finished.connect(_on_animation_finished)
 	hitbox.area_entered.connect(_on_hit)
 	despawn_timer.timeout.connect(despawn)
 	despawn_timer.start(DESPAWN_TIME)
@@ -28,7 +33,11 @@ func _on_hit(area):
 			StunnedEffect.new(0.25),
 		]
 		area.damage(projectile_attack)
-		despawn()
+		animation_player.play("pop")
+
+func _on_animation_finished(anim_name):
+	match anim_name:
+		"pop": despawn()
 
 func despawn():
 	queue_free()
