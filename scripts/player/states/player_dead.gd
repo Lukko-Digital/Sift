cd6ad331a88fc55d289a6bce32e0fd2dad2233c3
Ian Lum <ianlum314@gmail.com>
2023-12-27 16:17:45 -0800

    monsters dont detect player on player death do so by disabling collision box

--- scripts/player/states/player_dead.gd
+++ scripts/player/states/player_dead.gd
@@ -2,7 +2,9 @@ extends State
 
 @export var animation_player: AnimationPlayer
 
+@onready var collision_box: CollisionShape2D = character.get_node("CollisionBox")
+
 func enter():
 	animation_tree["parameters/playback"].travel("Die")
 	animation_tree["parameters/Die/blend_position"] = 1
-
+	collision_box.disabled = true
