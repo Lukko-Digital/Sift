32de8f656e2956e095b6d944a5355e1bd996b666
Ian Lum <ianlum314@gmail.com>
2023-12-27 16:42:39 -0800

    player respawns by resetting scene

--- scripts/player/states/player_dead.gd
+++ scripts/player/states/player_dead.gd
@@ -1,10 +1,11 @@
 extends State
 
 @export var animation_player: AnimationPlayer
-
 @onready var collision_box: CollisionShape2D = character.get_node("CollisionBox")
 
 func enter():
 	animation_tree["parameters/playback"].travel("Die")
 	animation_tree["parameters/Die/blend_position"] = 1
 	collision_box.disabled = true
+	await get_tree().create_timer(animation_player.get_animation("die_left").length).timeout
+	get_tree().reload_current_scene()
