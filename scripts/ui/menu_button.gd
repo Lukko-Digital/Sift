719279045e0d773a6e7b62eb8c72519a55fa019e
Ian Lum <ianlum314@gmail.com>
2023-12-28 23:16:24 -0800

    fully animate menu button

--- scripts/ui/menu_button.gd
+++ scripts/ui/menu_button.gd
@@ -12,3 +12,19 @@ func _ready():
 
 func _process(delta):
 	pass
+
+
+func _on_mouse_entered():
+	animation_player.play("hover")
+
+
+func _on_mouse_exited():
+	animation_player.stop()
+
+
+func _on_button_down():
+	animation_player.play("pressed")
+
+
+func _on_button_up():
+	animation_player.play("released")
